d21d4d7874aeee48046d1e3ca7d0b00101666930
alexwebbb <alexwebbb@yahoo.com>
2016-09-29 18:14:36 -0500

    first commit

---
+++ Assets/Scripts/VirtualJoystick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler {
+
+    private Image bgImg;
+    private Image joystickImg;
+    private Vector3 inputVector;
+
+    void Start() {
+        bgImg = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public virtual void OnDrag(PointerEventData ped) {
+
+    }
+
+	public virtual void OnPointerDown(PointerEventData ped) {
+        OnDrag(ped);
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped) {
+
+    }
+
+
+
+}

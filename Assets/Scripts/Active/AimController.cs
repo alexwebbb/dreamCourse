b05a98659f4f1ff3bb4257032d7d5c470d3d6038
Alex Webb <alexwebbb@yahoo.com>
2017-02-23 21:21:00 -0600

    set up continuous press feature

--- Assets/Scripts/Active/AimController.cs
+++ Assets/Scripts/Active/AimController.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class AimController : MonoBehaviour {
 
     public float movementInterval = 15;
+    public float heldButtonInterval = 0.05f;
 
     Transform verticalAxis;
     Vector3 left;
     Vector3 right;
     Vector3 down;
     Vector3 up;
+
+    float heldTime;
 
 	void Start () {
 
@@ -26,19 +29,24 @@ public class AimController : MonoBehaviour {
 	void Update () {
 
 
+        heldTime = heldTime + Time.deltaTime;
+
         // rotates this transform horizongtally	
-        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+        if ((Input.GetAxisRaw("Horizontal") < -0.5f  && heldTime > heldButtonInterval) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
             transform.Rotate(left);
-        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            heldTime = 0.0f;
+        } else if ((Input.GetAxisRaw("Horizontal") > 0.5f && heldTime > heldButtonInterval) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
             transform.Rotate(right);
+            heldTime = 0.0f;
         }
 
         // rotates this transform vertically. The limits are hard coded, this could perhaps change
-        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+        if ((Input.GetAxisRaw("Vertical") < -0.5f && heldTime > heldButtonInterval) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
             if(verticalAxis.eulerAngles.z > 10f) verticalAxis.Rotate(down);
-        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            heldTime = 0.0f;
+        } else if ((Input.GetAxisRaw("Vertical") > 0.5f && heldTime > heldButtonInterval) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
             if(verticalAxis.eulerAngles.z < 70f) verticalAxis.Rotate(up);
+            heldTime = 0.0f;
         }
-
     }
 }

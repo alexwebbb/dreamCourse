e1c6672b3d6f6c3e507135412800a1c103b3c5f9
Alex Webb <alexwebbb@yahoo.com>
2017-04-16 14:48:00 -0500

    sort of have bouncey water working

--- Assets/Scripts/Active/Level Components/WaterPad.cs
+++ Assets/Scripts/Active/Level Components/WaterPad.cs
@@ -9,6 +9,14 @@ public class WaterPad : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
 
+
+        Rigidbody otherRB = other.GetComponent<Rigidbody>();
+        // so this KIND OF works right now, need ton add particle effects and clean up the if second case
+        if (Vector3.Dot(Vector3.forward, otherRB.velocity.normalized) >0.8f) {
+            Debug.Log("bounce");
+            if(otherRB.velocity.x > -otherRB.velocity.y || otherRB.velocity.z > -otherRB.velocity.y) otherRB.AddForce(2f * Vector3.up * -otherRB.velocity.y, ForceMode.VelocityChange);
+        }
+
         BounceController bc = other.GetComponent<BounceController>();
 
         bc.AddDrag = resistance;

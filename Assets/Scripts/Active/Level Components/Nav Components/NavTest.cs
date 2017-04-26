8ed62117ab46188e9db8e4c7feb362625ec1efc4
Alex Webb <alexwebbb@yahoo.com>
2017-04-25 20:37:05 -0500

    fixed up the checkerboard prefabs, starting with nav stuff

---
+++ Assets/Scripts/Active/Level Components/Nav Components/NavTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavTest : MonoBehaviour {
+
+    public List<Transform> goal;
+
+    int active = 0;
+    NavMeshAgent agent;
+
+    void Start() {
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal[0].position;
+    }
+
+    void Update() {
+        
+        float dist = agent.remainingDistance;
+        if (dist != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0) {
+            if(active < goal.Count - 1) {
+                active++;
+            } else {
+                active = 0;
+            }
+            agent.destination = goal[active].position;
+        }
+    }
+}

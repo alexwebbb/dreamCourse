7c2af15310f60a230680e39e8595b121806d41a5
Alex Webb <alexwebbb@yahoo.com>
2017-04-30 01:05:15 -0500

    added splash effect

--- Assets/Scripts/Active/Level Components/WaterPad.cs
+++ Assets/Scripts/Active/Level Components/WaterPad.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WaterPad : MonoBehaviour {
 
+    public ParticleSystem splashEffect;
+
     public float power = 5f;
     public float resistance = 2f;
     public float skipThreshhold = 0.9f;
@@ -11,16 +13,16 @@ public class WaterPad : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
 
-
-        // I think this pretty much works now, need to add particle effects
+        //// surface bounce
         Rigidbody otherRB = other.GetComponent<Rigidbody>();
-        // Debug.Log(Mathf.Abs(Vector3.Dot(new Vector3(otherRB.velocity.x, 0, otherRB.velocity.z).normalized, otherRB.velocity.normalized)));
         // check the dot product of the velocity vs the horizontal plane
         if (Mathf.Abs(Vector3.Dot(new Vector3(otherRB.velocity.x, 0, otherRB.velocity.z).normalized, otherRB.velocity.normalized)) > skipThreshhold) {
             // compare the magnitude of horizontal movement vs vertical, if horizontal is greater, bounce can occur
             if (Vector3.SqrMagnitude(new Vector3(otherRB.velocity.x, 0, otherRB.velocity.z))/2 > Vector3.SqrMagnitude(new Vector3(0, otherRB.velocity.y, 0))) {
                 // adjust velocity, slowing horizontal movement, adding vertical velocity
                 otherRB.velocity = new Vector3(otherRB.velocity.x / 2, skipPower * -otherRB.velocity.y, otherRB.velocity.z / 2);
+                // splash effect
+                Destroy(Instantiate<GameObject>(splashEffect.gameObject, other.transform.position, splashEffect.transform.rotation), 1f);
             }
         }
 

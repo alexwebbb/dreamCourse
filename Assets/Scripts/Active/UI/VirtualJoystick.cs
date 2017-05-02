cab21a215d00941c46d3affafd391d75556d1524
Alex Webb <alexwebbb@yahoo.com>
2017-05-02 12:37:25 -0500

    refactored virtual joystick so it isnt stupid

--- Assets/Scripts/Active/UI/VirtualJoystick.cs
+++ Assets/Scripts/Active/UI/VirtualJoystick.cs
@@ -8,11 +8,11 @@ public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, I
     public float timeToReturn = .1f;
 
     public float GetHorizontal { get { return inputVector.x; } }
-    public float GetVertical { get { return inputVector.z; } }
+    public float GetVertical { get { return inputVector.y; } }
 
     Image bgImg;
     Image joystickImg;
-    Vector3 inputVector;
+    Vector2 inputVector;
 
     void Start() {
         bgImg = GetComponent<Image>();
@@ -24,31 +24,22 @@ public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, I
         // checks if you are clicking within the rect transform of the joystick
         if(RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos)) {
 
-            Debug.Log("pos x: " + pos.x + ", size delta x: " + bgImg.rectTransform.sizeDelta.x);
             // position gives the coordinates within the rectangle, in relation to the pivot
             // size delta is used to convert the raw position into a 0 to 1 value
             pos.x /= bgImg.rectTransform.sizeDelta.x;
             pos.y /= bgImg.rectTransform.sizeDelta.y;
 
-            Debug.Log("x pivot: " + bgImg.rectTransform.pivot.x + ", pos x after div: " + pos.x);
             // the formula used below converts the normalized pos into a value between -1 and 1
-            // the ternary operator is being used in case you for some reason you needed to change the pivot to 0
-            // however, the pivot is totally arbitrary as long as the math is correct, so the extra case is
-            // totally unnecessary, and confusing to have in there
-            //
-            // going to save this in git and then refactor this
-            float x = (bgImg.rectTransform.pivot.x == 1) ? pos.x * 2 + 1 : pos.x * 2 - 1;
-            float y = (bgImg.rectTransform.pivot.y == 1) ? pos.y * 2 + 1 : pos.y * 2 - 1;
+            float x = pos.x * 2 - 1;
+            float y = pos.y * 2 - 1;
 
-            inputVector = new Vector3(x, 0f, y);
-            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
-            Debug.Log("input vector : " + inputVector);
-
+            // input vector is accessed by properties
+            inputVector = new Vector2(x, y);
+            // if the inputVector magnitude is greater than 1, return the normalized vector
+            inputVector = (inputVector.sqrMagnitude > 1.0f) ? inputVector.normalized : inputVector;
+            
             // Move Joystick Image
-
-            joystickImg.rectTransform.anchoredPosition = new Vector3(inputVector.x * (bgImg.rectTransform.sizeDelta.x / 3), inputVector.z * (bgImg.rectTransform.sizeDelta.x / 3));
-
-            // Debug.Log(inputVector);
+            joystickImg.rectTransform.anchoredPosition = new Vector3(inputVector.x * (bgImg.rectTransform.sizeDelta.x / 3), inputVector.y * (bgImg.rectTransform.sizeDelta.x / 3));
         }
 
     }
@@ -71,7 +62,6 @@ public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, I
         while(elapsedTime < timeToReturn) {
             joystickImg.rectTransform.anchoredPosition = Vector2.Lerp(startPosition, Vector2.zero, elapsedTime / timeToReturn);
             elapsedTime += Time.deltaTime;
-            // Debug.Log(elapsedTime);
             yield return new WaitForEndOfFrame();
         }
 

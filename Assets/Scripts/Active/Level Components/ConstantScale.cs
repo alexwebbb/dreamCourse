0a7ea37d45ad8191a9e0de2aff8dac5e68e6a636
Alex Webb <alexwebbb@yahoo.com>
2017-05-14 14:19:02 -0500

    starting on scaler

---
+++ Assets/Scripts/Active/Level Components/ConstantScale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantScale : MonoBehaviour {
+
+    public float minScale;
+    public float maxScale;
+
+    bool active;
+    Transform referencePoint;
+
+
+}
